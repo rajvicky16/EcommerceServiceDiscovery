a25aba9b710933bd5a61a0d1b1d640dd372d0a9a
rajashanmugam <rajvicky16721@gmail.com>
2025-01-12 20:27:12 +0530

    initial commit service discovery

---
+++ src/main/java/com/ecommerce/ecommerceservicediscovery/EcommerceServiceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce.ecommerceservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EcommerceServiceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceServiceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/ecommerceservicediscovery/EcommerceServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerceservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
